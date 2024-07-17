a0548cc5bc370b772da8b07479e024574ed7a594
Isaque Scheidegger <isaquescheidegger@gmail.com>
2024-07-17 16:30:55 -0300

    Merge branch 'main' of https://github.com/IsaqueScheidegger/exception1-java
    # Conflicts:
    #	.gitignore

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import Entities.Services.*;
+
 public class Program {
-    public static void main (String[] args){
+
+    public static void main(String[] args) {
+
+        SelectMenu.showMenu();
 
     }
-
 }
